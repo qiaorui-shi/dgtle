d43767091fa9f8daae4e138e36b6489c9b2904a5
shiqiaorui <2467383712@qq.com>
2025-08-11 20:08:45 +0800

    build(docker): 优化Dockerfile配置并更新应用启动方式

--- dgtle-service/src/app.module.ts
+++ dgtle-service/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import dbconfig from '@/config/index';
+import config from '@/config/index';
 import { DatabaseModule } from '@/db/mysql/db.module';
 import { RedisModule } from '@/db/redis/redis.module';
 import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
@@ -14,7 +14,7 @@ import { DynamicModule } from '@/module/dynamic/dynamic.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [dbconfig],
+      load: [config],
       cache: true,
     }),
     DatabaseModule,
